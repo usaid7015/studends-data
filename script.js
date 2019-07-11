4bc72024958120069c9e723ba7c531eb16c05517
usaid7015 <49516724+usaid7015@users.noreply.github.com>
2019-07-11 14:59:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+let studentsTechKaro = [
+    {
+        name: "asad",
+        age: 16,
+        qualification: "matric",
+        number: 0354535345,
+        hobbies: "Study",
+        height: 56,
+        hobbies: "criket",
+    },
+    {
+        name: "Naeem",
+        age: 19,
+        qualification: "B.A",
+        number: 03654646566,
+        height: 21,
+        hobbies: "Study", 
+    },
+    {
+        name: "umar",
+        age: 12,
+        height: 34,
+        qualification: "enter",
+        number: 0313676332565,
+        hobbies: "criket",
+     
+    },
+    {
+        name: "ismail",
+        age: 23,
+        qualification: "C.A",
+        number: 0313334545,
+        hobbies: "criket",
+     
+    },
+    {
+        name: "usama",
+        age: 27,
+        qualification: "B.B.A",
+        number: 42342342565,
+        hobbies: "criket",
+    },
+    {
+        name: "ali",
+        age: 29,
+        qualification: "M.B.A",
+        number: 2342682565,
+        hobbies: "criket",
+    },
+    {
+        name: "Amaan",
+        age: 30,
+        qualification: "B.Com.",
+        number: 3213182565,
+        hobbies: "criket",
+    },
+];
